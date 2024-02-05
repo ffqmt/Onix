c9f6f8969253366ee623519f72f8b07859d1bb39
onixsolutionscontaudi <onixsolutionscontaudi@gmail.com>
2024-02-05 18:21:04 -0400

    up

--- server-waitress-5000.py
+++ server-waitress-5000.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     try:
         carregarAgendamentosAnteriores()
         scheduler.start()
+        print('Servindo OnixWeb')
         serve(app, host='0.0.0.0', port=5000, threads=8)
         atexit.register(lambda: scheduler.shutdown())
     except Exception as e:
